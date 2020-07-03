b585cefc69d6400be6f2c4b31cd89c69b464777c
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 09:28:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine(); // citim primul număr
+            decimal a = Convert.ToDecimal(inputData);
+            bool b = true;
+            if (a % 2 == 0)
+            {
+                b = false;
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
